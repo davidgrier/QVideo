646395acc154df6766359ea84df4b263e9e4e198
davidgrier <david.grier@nyu.edu>
2021-09-03 15:59:12 -0400

    first implementation

---
+++ QSpinnakerWidget.py
@@ -0,0 +1,25 @@
+from QCameraWidget import QCameraWidget
+from QSpinnakerCamera import QSpinnakerCamera
+
+
+class QSpinnakerWidget(QCameraWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,
+                         camera=QSpinnakerCamera(),
+                         uiFile='QSpinakerWidget.ui',
+                         **kwargs)
+
+
+def main():
+    from PyQt5.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    widget = QSpinnakerWidget()
+    widget.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()

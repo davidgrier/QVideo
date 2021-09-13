04104eaaec1229d7da08512cc14b1af280b852b8
David Grier <david.grier@nyu.edu>
2021-09-12 20:40:30 -0400

    automate property definition

--- cameras/Spinnaker/QSpinnakerInterface.py
+++ cameras/Spinnaker/QSpinnakerInterface.py
@@ -62,8 +62,8 @@ class QSpinnakerInterface(QVideoCamera):
         super().__init__(*args, **kwargs)
 
         self.open(cameraID)
+        self.width = self._property('Width')
 
-   
     def open(self, index=0):
         '''
         Initialize Spinnaker and open specified camera
@@ -136,141 +136,50 @@ class QSpinnakerInterface(QVideoCamera):
             return False, None
         return True, img.GetNDArray()
 
-    #
-    # private methods for handling interactions with GenICam
-    #
-    def _add_property(self, fname, stop=False):
+    def _property(self, name, stop=False, notify=None):
+
+        dtype = {PySpin.infIBoolean: bool,
+                 PySpin.intfIInteger: int,
+                 PySpin.intfIFloat: float,
+                 PySpin.intfIString: str,
+                 PySpin.intfIEnumeration: str}
+        try:
+            feature = getattr(self.device, name)
+        except AttributeError:
+            logger.warning(f'Camera does not have feature {fname}')
+            return None
+        if not PySpin.IsAvailable(feature):
+            logger.warning(f'{fname} is not available')
+            return None
+        if not PySpin.IsReadable(feature):
+            logger.warning(f'{fname} is not readable')
+            return None
+        if not PySpin.IsWritable(feature):
+            logger.warning(f'{fname} is not writable')
+            return None
+
+        iface = feature.GetPrincipalInterfaceType()
+        is_enum = iface == PySpin.intfIEnumeration
+        type = dtype[iface]
+
         def getter(self):
-            return self._get_feature(fname)
+            get = feature.ToString if is_enum else feature.Getvalue
+            return get()
 
         @QVideoCamera.protected
-        def setter(self, value, stop=stop):
-            self._set_feature(fname, value)
-
-        dtype = self._feature_type(fname)
-        return pyqtProperty(dtype, getter, setter)
-
-    _fmap = {PySpin.intfICategory: PySpin.CCategoryPtr,
-             PySpin.intfIString: PySpin.CStringPtr,
-             PySpin.intfIInteger: PySpin.CIntegerPtr,
-             PySpin.intfIFloat: PySpin.CFloatPtr,
-             PySpin.intfIBoolean: PySpin.CBooleanPtr,
-             PySpin.intfICommand: PySpin.CCommandPtr,
-             PySpin.intfIEnumeration: PySpin.CEnumerationPtr}
-
-    _tmap = {PySpin.intfICategory: str,
-             PySpin.intfIString: str,
-             PySpin.intfIInteger: int,
-             PySpin.intfIFloat: float,
-             PySpin.intfIBoolean: bool,
-             PySpin.intfICommand: str,
-             PySpin.intfIEnumeration: str}
-
-    def _feature(self, fname):
-        '''Return inode for named feature'''
-        feature = None
-        try:
-            node = self._nodes.GetNode(fname)
-            type = node.GetPrincipalInterfaceType()
-            feature = self._fmap[type](node)
-        except Exception as ex:
-            logger.warning(f'Could not access Property: {fname} {ex}')
-        return feature
-
-    def _feature_type(self, fname):
-        node = self._nodes.GetNode(fname)
-        type = node.GetPrincipalInterfaceType()
-        return self._tmap[type]
-
-    def _get_feature(self, fname):
-        value = None
-        feature = self._feature(fname)
-        if self._is_category(feature):
-            nodes = feature.GetFeatures()
-            names = [node.GetName() for node in nodes]
-            value = {name: self._get_feature(name) for name in names}
-        elif self._is_enum(feature) or self._is_command(feature):
-            value = feature.ToString()
-        elif self._is_readable(feature):
-            value = feature.GetValue()
-        logger.debug(f'Getting {fname}: {value}')
-        return value
-
-    def _get_features(self):
-        '''Return dict of camera inodes and values'''
-        return self._get_feature('Root')
-
-    def _set_feature(self, fname, value):
-        logger.debug(f'Setting {fname}: {value}')
-        feature = self._feature(fname)
-        if not self._is_writable(feature):
-            logger.warning(f'Property {fname} is not writable')
-            return
-        try:
-            if self._is_enum(feature) or self._is_command(feature):
-                feature.FromString(value)
+        def setter(self, value):
+            set = feature.FromString if is_enum else feature.SetValue
+            if stop and self._running:
+                self.endAcquisition()
+                set(value)
+                self.beginAcquisition()
             else:
-                feature.SetValue(value)
-        except PySpin.SpinnakerException as ex:
-            logger.warning(f'Could not set {fname}: {ex}')
+                set(value)
+            if notify is not None:
+                notify.emit()
 
-    def _feature_range(self, fname):
-        '''Return minimum and maximum values of named feature'''
-        feature = self._feature(fname)
-        try:
-            range = (feature.GetMin(), feature.GetMax())
-        except PySpin.SpinnakerException as ex:
-            logger.warning(f'Could not get range of {fname}: {ex}')
-            range = None
-        return range
-
-    def _is_readwrite(self, feature):
-        return self._is_readable(feature) and self._is_writable(feature)
-
-    def _is_readonly(self, feature):
-        return self._is_readable(feature) and not self._is_writable(feature)
-
-    def _is_writeonly(self, feature):
-        return self._is_writable(feature) and not self._is_readable(feature)
-    
-    def _is_readable(self, feature):
-        return PySpin.IsAvailable(feature) and PySpin.IsReadable(feature)
-
-    def _is_writable(self, feature):
-        return PySpin.IsAvailable(feature) and PySpin.IsWritable(feature)
-
-    def _is_type(self, feature, typevalue):
-        return (self._is_readable(feature) and
-                feature.GetPrincipalInterfaceType() == typevalue)
-
-    def _is_category(self, feature):
-        return self._is_type(feature, PySpin.intfICategory)
-
-    def _is_enum(self, feature):
-        return self._is_type(feature, PySpin.intfIEnumeration)
-
-    def _is_command(self, feature):
-        return self._is_type(feature, PySpin.intfICommand)
-
-    #
-    # Methods for introspection
-    #
-
-    def transport_info(self):
-        '''Return dict of Transport Layer Device inodes and values'''
-        nodemap = self.device.GetTLDeviceNodeMap()  # Transport layer
-        try:
-            info = PySpin.CCategoryPtr(nodemap.GetNode('DeviceInformation'))
-            if self._is_readable(info):
-                features = info.GetFeatures()
-                for feature in features:
-                    this = PySpin.CValuePtr(feature)
-                    logger.info(f'{this.GetName()}: {this.ToString()}')
-                else:
-                    logger.warning('Device control information not available')
-        except PySpin.SpinnakerException as ex:
-            logger.warning('{}'.format(ex))
-
+        return pyqtProperty(type, getter, setter)
+                
 
 def main():
     import json

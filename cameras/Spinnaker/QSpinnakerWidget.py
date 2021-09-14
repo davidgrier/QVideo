e057101bad2cc3fc2048d8f616defc6392aa2dd1
davidgrier <david.grier@nyu.edu>
2021-09-13 21:33:56 -0400

    set ranges

--- cameras/Spinnaker/QSpinnakerWidget.py
+++ cameras/Spinnaker/QSpinnakerWidget.py
@@ -9,6 +9,22 @@ class QSpinnakerWidget(QCameraWidget):
                          camera=QSpinnakerCamera(),
                          uiFile='QSpinnakerWidget.ui',
                          **kwargs)
+        self.setRanges()
+        self.connectSignals()
+
+    def setRanges(self):
+        cam = self.camera
+        self.ui.acquisitionframerate.setRange(*cam.acquisitionframeraterange)
+        self.ui.exposuretime.setRange(*cam.exposuretimerange)
+        self.ui.gain.setRange(*cam.gainrange)
+        self.ui.gamma.setRange(*cam.gammarange)
+        self.ui.height.setRange(*cam.heightrange)
+        self.ui.offsetx.setRange(*cam.offsetxrange)
+        self.ui.offsety.setRange(*cam.offsetyrange)
+        self.ui.width.setRange(*cam.widthrange)
+        self.update()
+
+    def connectSignals(self):
         self.camera.fpsReady.connect(self.ui.actualrate.setValue)
 
 

4c189a606479e6831c5bfebaf8dc2ff2a55f1727
David Grier <david.grier@nyu.edu>
2022-11-21 08:08:01 -0500

    sample-and-hold filter

---
+++ filters/SampleHold.py
@@ -0,0 +1,29 @@
+from QVideo.filters import Normalize
+import numpy as np
+
+
+class SampleHold(Normalize):
+
+    '''Normalize image by a previously sampled background estimate
+
+    Inherits
+    --------
+    QVideo.filters.Normalize
+
+    Methods
+    -------
+    reset(): None
+        Recompute the background estimate
+    '''
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.reset()
+
+    def reset(self) -> None:
+        self.count = 3**self.order
+
+    def add(self, data: np.ndarray) -> None:
+        if self.count > 0:
+            super().add(data)
+            self.count -= 1

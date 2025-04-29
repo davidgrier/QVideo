03fe6b06ac5d09955cbc04330cf2e7112202bc5f
David Grier <david.grier@nyu.edu>
2025-04-29 16:37:13 -0400

    add flir support

--- cameras/choose_camera.py
+++ cameras/choose_camera.py
@@ -10,50 +10,64 @@ logger.setLevel(logging.WARNING)
 __all__ = 'choose_camera choose_camera_widget'.split()
 
 
-def camera_parser(parser=None):
+def camera_parser(parser: ArgumentParser | None = None) -> ArgumentParser:
     parser = parser or ArgumentParser()
     arg = parser.add_argument
     arg('-c', dest='opencv', help='OpenCV camera', action='store_true')
-    arg('-s', dest='spinnaker', help='Spinnaker camera', action='store_true')
+    arg('-f', dest='flir', help='Flir camera', action='store_true')
+    arg('-s', dest='spinnaker', help='Spinnaker SDK', action='store_true')
     return parser
 
 
-def parse_command_line(parser=None):
-    parser = camera_parser(parser)
-    return parser.parse_known_args()
-
-
-def choose_camera():
-    args, _ = parse_command_line()
+def choose_camera(parser: ArgumentParser | None = None) -> ArgumentParser:
+    args, _ = camera_parser(parser).parse_known_args()
     if args.opencv:
         try:
             from QVideo.cameras.OpenCV import QOpenCVCamera
             return QOpenCVCamera
         except ImportError as ex:
             logger.warning(f'Could not import OpenCV camera: {ex}')
+
+    if args.flir:
+        try:
+            from QVideo.cameras.Flir import QFlirCamera
+            return QFlirCamera
+        except ImportError as ex:
+            logger.warning(f'Could not import Flir camera: {ex}')
+
     if args.spinnaker:
         try:
             from QVideo.cameras.Spinnaker import QSpinnakerCamera
             return QSpinnakerCamera
         except ImportError as ex:
             logger.warning(f'Could not import Spinnaker camera: {ex}')
+
     from QVideo.cameras.Noise import QNoiseSource
     return QNoiseSource
 
 
-def choose_camera_widget():
-    args, _ = parse_command_line()
+def choose_camera_widget(parser: ArgumentParser | None = None) -> ArgumentParser:
+    args, _ = camera_parser(parser).parse_known_args()
     if args.opencv:
         try:
             from QVideo.cameras.OpenCV import QOpenCVTree
             return QOpenCVTree
         except ImportError as ex:
             logger.warning(f'Could not import OpenCV widget: {ex}')
+
+    if args.flir:
+        try:
+            from QVideo.cameras.Flir import QFlirTree
+            return QFlirTree
+        except ImportError as ex:
+            logger.warning(f'COuld not import Flir widget: {ex}')
+
     if args.spinnaker:
         try:
             from QVideo.cameras.Spinnaker import QSpinnakerTree
             return QSpinnakerTree
         except ImportError as ex:
             logger.warning(f'Could not import Spinnaker widget: {ex}')
+
     from QVideo.cameras.Noise import QNoiseTree
     return QNoiseTree

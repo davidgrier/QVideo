41234790c470192ef2a0b4e26cdd3e0cefdf29d2
David Grier <david.grier@nyu.edu>
2025-11-16 20:57:12 -0500

    FPS meter is callable

--- lib/QFPSMeter.py
+++ lib/QFPSMeter.py
@@ -1,9 +1,43 @@
-from pyqtgraph.Qt.QtCore import (QObject, pyqtSignal, pyqtSlot,
-                                 pyqtProperty)
+from pyqtgraph.Qt.QtCore import (QObject,
+                                 pyqtSignal, pyqtSlot, pyqtProperty)
 import time
 
 
 class QFPSMeter(QObject):
+    '''A frames-per-second (FPS) meter that calculates
+    the frame rate over a specified window of frames.
+
+    Inherits
+    --------
+    QObject
+        The base class for all Qt objects.
+
+    Parameters
+    ----------
+    window : int
+        The number of frames over which to calculate the FPS.
+
+    Returns
+    -------
+    QFPSMeter : QObject
+        The FPS meter object.
+
+    Signals
+    -------
+    fpsReady(float)
+        Emitted when a new FPS value is calculated.
+
+    Slots
+    -----
+    tick() -> None
+        Increment the frame count and calculate FPS if
+        the window size is reached.
+
+    Properties
+    ----------
+    value : float
+        The current FPS value.
+    '''
 
     fpsReady = pyqtSignal(float)
 
@@ -14,6 +48,9 @@ class QFPSMeter(QObject):
         self._value = 0.
         self.count = 0
         self.start = time.time()
+
+    def __call__(self) -> float:
+        return self.value
 
     @pyqtSlot()
     def tick(self) -> None:
